6a1cb483c1192dee1969ead63268506c4ab55de1
Septian Cahya <cahyaseptian39@gmail.com>
2023-03-09 23:01:40 +0700

    fix path image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        "hero-bg": "url('public/images/hero-img.png')",
-        "about-bg": "url('public/images/about-img.png')",
+        "hero-bg": "url('/public/images/hero-img.png')",
+        "about-bg": "url('/public/images/about-img.png')",
       },
     },
   },
